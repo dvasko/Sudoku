0b9f7fe4ef641b703a1ebd7fba92d3d4749d2dfa
dvasko <dejan.vasko@gmail.com>
2016-04-11 22:24:54 +0200

    sudoku generator

---
+++ app/src/main/java/com/vasko/sudoku/helper/SudokuGenerator.java
@@ -0,0 +1,19 @@
+package com.vasko.sudoku.helper;
+
+import com.vasko.sudoku.model.Point;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SudokuGenerator {
+
+    public enum Difficulty {
+        BEGINNER, ADVANCED, EXPERT
+    }
+
+    public static Map<Point, Integer> generate(Difficulty difficulty) {
+        //TODO someone will implement this method
+        return new HashMap<>();
+    }
+
+}
