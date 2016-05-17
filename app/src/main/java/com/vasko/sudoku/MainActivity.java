4717353b6b86c343d6f10e54b6b66701ca900167
dvasko <dejan.vasko@gmail.com>
2016-05-17 15:07:45 +0200

    generator

--- app/src/main/java/com/vasko/sudoku/MainActivity.java
+++ app/src/main/java/com/vasko/sudoku/MainActivity.java
@@ -11,12 +11,12 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
+import com.vasko.sudoku.helper.SudokuGenerator;
 import com.vasko.sudoku.helper.ToastBelowView;
 import com.vasko.sudoku.model.Keyboard;
-import com.vasko.sudoku.model.Point;
 import com.vasko.sudoku.model.Sudoku;
 
-import java.util.HashMap;
+import static com.vasko.sudoku.helper.SudokuGenerator.Difficulty.ADVANCED;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         sudoku = new Sudoku.Builder()
                 .savedInstance(savedInstanceState)
                 .layout(sudokuContainer)
-                .initial(getMap())
+                .initial(SudokuGenerator.generate(ADVANCED))
                 .build();
 
         ViewGroup keyboardContainer = (ViewGroup) findViewById(R.id.keyboard_container);
@@ -81,57 +81,5 @@ public class MainActivity extends AppCompatActivity {
         sudoku.onSavedInstanceState(outState);
         super.onSaveInstanceState(outState);
     }
-
-    private HashMap<Point, Integer> getMap() {
-        HashMap<Point, Integer> initial = new HashMap<>();
-        initial.put(new Point(1, 1), 7);
-        initial.put(new Point(2, 1), 3);
-        initial.put(new Point(4, 1), 5);
-        initial.put(new Point(5, 1), 6);
-
-        initial.put(new Point(1, 2), 2);
-        initial.put(new Point(3, 2), 1);
-        initial.put(new Point(4, 2), 7);
-        initial.put(new Point(6, 2), 8);
-        initial.put(new Point(9, 2), 6);
-
-        initial.put(new Point(3, 3), 8);
-        initial.put(new Point(4, 3), 3);
-        initial.put(new Point(6, 3), 9);
-
-
-        initial.put(new Point(1, 4), 3);
-        initial.put(new Point(2, 4), 4);
-        initial.put(new Point(3, 4), 6);
-        initial.put(new Point(7, 4), 9);
-        initial.put(new Point(9, 4), 8);
-
-        initial.put(new Point(2, 5), 2);
-        initial.put(new Point(8, 5), 4);
-
-        initial.put(new Point(1, 6), 8);
-        initial.put(new Point(3, 6), 7);
-        initial.put(new Point(7, 6), 6);
-        initial.put(new Point(8, 6), 3);
-        initial.put(new Point(9, 6), 1);
-
-
-        initial.put(new Point(4, 7), 2);
-        initial.put(new Point(6, 7), 3);
-        initial.put(new Point(7, 7), 4);
-
-        initial.put(new Point(1, 8), 9);
-        initial.put(new Point(4, 8), 4);
-        initial.put(new Point(6, 8), 1);
-        initial.put(new Point(7, 8), 5);
-        initial.put(new Point(9, 8), 3);
-
-        initial.put(new Point(5, 9), 8);
-        initial.put(new Point(6, 9), 5);
-        initial.put(new Point(8, 9), 9);
-        initial.put(new Point(9, 9), 2);
-        return initial;
-    }
-
 
 }
--- app/src/main/java/com/vasko/sudoku/helper/SudokuGenerator.java
+++ app/src/main/java/com/vasko/sudoku/helper/SudokuGenerator.java
@@ -13,7 +13,54 @@ public class SudokuGenerator {
 
     public static Map<Point, Integer> generate(Difficulty difficulty) {
         //TODO someone will implement this method
-        return new HashMap<>();
+        HashMap<Point, Integer> initial = new HashMap<>();
+        initial.put(new Point(1, 1), 7);
+        initial.put(new Point(2, 1), 3);
+        initial.put(new Point(4, 1), 5);
+        initial.put(new Point(5, 1), 6);
+
+        initial.put(new Point(1, 2), 2);
+        initial.put(new Point(3, 2), 1);
+        initial.put(new Point(4, 2), 7);
+        initial.put(new Point(6, 2), 8);
+        initial.put(new Point(9, 2), 6);
+
+        initial.put(new Point(3, 3), 8);
+        initial.put(new Point(4, 3), 3);
+        initial.put(new Point(6, 3), 9);
+
+
+        initial.put(new Point(1, 4), 3);
+        initial.put(new Point(2, 4), 4);
+        initial.put(new Point(3, 4), 6);
+        initial.put(new Point(7, 4), 9);
+        initial.put(new Point(9, 4), 8);
+
+        initial.put(new Point(2, 5), 2);
+        initial.put(new Point(8, 5), 4);
+
+        initial.put(new Point(1, 6), 8);
+        initial.put(new Point(3, 6), 7);
+        initial.put(new Point(7, 6), 6);
+        initial.put(new Point(8, 6), 3);
+        initial.put(new Point(9, 6), 1);
+
+
+        initial.put(new Point(4, 7), 2);
+        initial.put(new Point(6, 7), 3);
+        initial.put(new Point(7, 7), 4);
+
+        initial.put(new Point(1, 8), 9);
+        initial.put(new Point(4, 8), 4);
+        initial.put(new Point(6, 8), 1);
+        initial.put(new Point(7, 8), 5);
+        initial.put(new Point(9, 8), 3);
+
+        initial.put(new Point(5, 9), 8);
+        initial.put(new Point(6, 9), 5);
+        initial.put(new Point(8, 9), 9);
+        initial.put(new Point(9, 9), 2);
+        return initial;
     }
 
 }
